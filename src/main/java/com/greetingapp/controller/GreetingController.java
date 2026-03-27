f08e1944372c814af9b82577c6d691c714ef9c9e
Barnamwork <barnamwork@gmail.com>
2026-03-28 02:30:22 +0530

    [Barnam] UC2: Added GreetingService layer to return Hello World message

--- src/main/java/com/greetingapp/controller/GreetingController.java
+++ src/main/java/com/greetingapp/controller/GreetingController.java
@@ -1,30 +1,19 @@
 package com.greetingapp.controller;
 
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Map;
+import com.greetingapp.service.GreetingService;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    @Autowired
+    GreetingService service;
+
     @GetMapping
     public Map<String, String> getGreeting() {
-        return Map.of("message", "Hello from GET");
-    }
-
-    @PostMapping
-    public Map<String, String> postGreeting() {
-        return Map.of("message", "Hello from POST");
-    }
-
-    @PutMapping
-    public Map<String, String> putGreeting() {
-        return Map.of("message", "Hello from PUT");
-    }
-
-    @DeleteMapping
-    public Map<String, String> deleteGreeting() {
-        return Map.of("message", "Hello from DELETE");
+        return Map.of("message", service.getGreeting());
     }
 }
